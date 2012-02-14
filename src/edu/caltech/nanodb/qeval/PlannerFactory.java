a494af0eea6e2a840fb7fa6074e79ffc1b71cf7d
Atharv Vaish <avaish@caltech.edu>
2012-02-13 23:33:57 -0800

    Switch the default planner from SimplePlanner to the new DPJoinPlanner.

--- src/edu/caltech/nanodb/qeval/PlannerFactory.java
+++ src/edu/caltech/nanodb/qeval/PlannerFactory.java
@@ -14,7 +14,7 @@ public class PlannerFactory {
      * overridden in the configuration.
      */
     public static final String DEFAULT_PLANNER =
-        "edu.caltech.nanodb.qeval.SimplePlanner";
+        "edu.caltech.nanodb.qeval.DPJoinPlanner";
 
 
     private PlannerFactory() {
